91b231a07d2cf4377a95ffcfe1b26b893cd2ae25
max <enriquelongton@gmail.com>
2019-09-30 23:55:25 -0400

    added switch case to run

--- fulcrum_electronics.py
+++ fulcrum_electronics.py
@@ -53,11 +53,14 @@ class MotorController(PCA9685, Thread):
             # print(value)
             sleep(self.sampleFreq)
         while self.started:
-            if self.controller == 1:
+            if self.controller == 0:
+                self.setThrottle(self.startupValue - 300)
+            elif self.controller == 1:
                 self.setThrottle(self.startupValue)
+            elif self.controller == 2:
+                self.setThrottle(self.startupValue + 100)
             else:
-                self.started = False
-
+                self.setThrottle(0)
             sleep(self.sampleFreq)
         print('run loop ended')
         
--- main.py
+++ main.py
@@ -13,53 +13,72 @@ db = DBManager()
 motorController = fe.MotorController(i2c_bus, 7, 500, sampleFreq, db.retrieve_fulcrum_values(id=1)[1]+500)
 
 #routines
-def options():
-        print('''
-        Select your choice:
-        (1): Quit
-        (2): Calibrate Sensor
-        (3): Set Throttle Range
-        (4): Fulcrum Prop Values
-        (5): Startup Motor
-        (6): Turn Off Motor
-        '''
-        )
-        optionSwitch()
+def options(step):
+        if step == 0:
+                print('''
+                Select your choice:
+                (1): Quit
+                (2): Calibrate Sensor
+                (3): Set Throttle Range
+                (4): Fulcrum Prop Values
+                (5): Startup Motor
+                (6): Kill Motor
+                '''
+                )
+                optionSwitch(0)
+        elif step == 1:
+                print('''
+                Select your choice:
+                (1): Manual Mode
+                (2): Pause Motor
+                '''
+                )
+                optionSwitch(1)
 
-def optionSwitch():
+def optionSwitch(step):
         choice = input('Choice: ')
-        if choice == '1':
-                print('exiting')
-                pass
-        elif choice == '2':
-                calibration = angleSensor.calibrate(sampleFreq)
-                db.update_calibration(calibration)
-                options()
-        elif choice == '3':
-                [lowerValue, upperValue] = motorController.setThrottleRange()
-                db.update_throttle_limits(lowerValue, upperValue)
-                options()
-        elif choice == '4':
-                fulcrumValues = db.retrieve_fulcrum_values(id=1)
-                print(f'\nCalibration Offset: {fulcrumValues[0]}')
-                print(f'Throttle Low: {fulcrumValues[1]}')
-                print(f'Throttle High: {fulcrumValues[2]}')
-                input('\nPress return to continue...')
-                options()
-        elif choice == '5':
-                # thread1 = fe.MotorController(i2c_bus, 7, 500)
-                motorController.start()
-                # x = threading.Thread(target=motorController.startup, args=(sampleFreq, db.retrieve_fulcrum_values(id=1)[1]+500))
-                # x.start()
-                options()
-        elif choice == '6':
-                motorController.stopMotor()
-                options()
-        else:
-                print('nothing selected, try again')
-                optionSwitch()
-
+        if step == 0:
+                if choice == '1':
+                        print('exiting')
+                        pass
+                elif choice == '2':
+                        calibration = angleSensor.calibrate(sampleFreq)
+                        db.update_calibration(calibration)
+                        options(0)
+                elif choice == '3':
+                        [lowerValue, upperValue] = motorController.setThrottleRange()
+                        db.update_throttle_limits(lowerValue, upperValue)
+                        options(0)
+                elif choice == '4':
+                        fulcrumValues = db.retrieve_fulcrum_values(id=1)
+                        print(f'\nCalibration Offset: {fulcrumValues[0]}')
+                        print(f'Throttle Low: {fulcrumValues[1]}')
+                        print(f'Throttle High: {fulcrumValues[2]}')
+                        input('\nPress return to continue...')
+                        options(0)
+                elif choice == '5':
+                        if motorController.is_alive():
+                                motorController.controller = 1
+                        else:
+                                motorController.start()
+                        options(1)
+                elif choice == '6':
+                        motorController.stopMotor()
+                        pass
+                else:
+                        print('nothing selected, try again')
+                        optionSwitch(0)
+        elif step == 1:
+                if choice == '1':
+                        motorController.controller = 2 #manual controller
+                        options(1)
+                elif choice == '2':
+                        motorController.controller = 0 #stop
+                        options(0)
+                else:
+                        print('nothing selected, try again')
+                        optionSwitch(0)
 def main():
-        options()
+        options(0)
         pass
 main()
