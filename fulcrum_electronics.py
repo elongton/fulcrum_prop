5a2f31e22a091dcf731285147883e78b80b6bbad
max <enriquelongton@gmail.com>
2019-09-30 22:53:18 -0400

    updated something

--- fulcrum_electronics.py
+++ fulcrum_electronics.py
@@ -3,7 +3,8 @@ from adafruit_pca9685 import PCA9685
 import statistics
 from time import sleep
 import time
-from threading import Thread
+import threading
+
 
 class Sensor(BNO055):
     def __init__(self, i2c):
@@ -34,28 +35,28 @@ class Sensor(BNO055):
     def sensedRate(self):
         return self.euler[3]
 
-class MotorController(PCA9685, Thread):
-    def __init__(self, i2c, motorChannel:int, pwmFrequency:int, sampleFreq:float, startupValue:int):
+class MotorController(PCA9685, threading.Thread):
+# class MotorController(PCA9685):
+    def __init__(self, i2c, motorChannel:int, pwmFrequency:int):
         PCA9685.__init__(self, i2c)
-        Thread.__init__(self)
+        threading.Thread.__init__(self)
+        self.i2c = i2c
         self.motorChannel = motorChannel
-        self.frequency = pwmFrequency
-        self.sampleFreq = sampleFreq
-        self.startupValue = startupValue
+        self.pwmFrequency = pwmFrequency
         self.startupSequence = False
-        
-    #override the Thread run method (this is the only one you can override)
+        self.started = True
+
+
+
     def run(self):
-        self.startupSequence = True
-        for x in range(100):
-            value = round(x/100 * self.startupValue)
-            self.channels[self.motorChannel].duty_cycle = value
-            # print(value)
-            sleep(self.sampleFreq)
-        while self.startupSequence:
-            self.channels[self.motorChannel].duty_cycle = self.startupValue
-            sleep(self.sampleFreq)
-        
+        print('starting')
+        i=0
+        while self.started:
+            i = i+30
+            # print(i)
+            self.setThrottle(i)
+            sleep(0.02)
+        print('finished')
 
     def stopMotor(self):
         self.startupSequence = False
@@ -64,14 +65,43 @@ class MotorController(PCA9685, Thread):
     def setThrottle(self, value:int):
         self.channels[self.motorChannel].duty_cycle = value
 
-    def setThrottleRange(self):
+    def setThrottleRange(self, sampleFreq):
         print('SET THROTTLE RANGE: ESC must be de-energized.')
         upperValue = input('SET THROTTLE RANGE: Enter upper value: ')
-        self.setThrottle(int(upperValue))
+        upperValueThread = threading.Thread(target=self.constantThrottle, args=(sampleFreq, int(upperValue)))
+        upperValueThread.start()
+        # self.setThrottle(int(upperValue))
         input('SET THROTTLE RANGE: Now energize the ESC and wait for beep.')
         lowerValue = input('SET THROTTLE RANGE: Enter lower value: ')
-        self.setThrottle(int(lowerValue))
+        upperValueThread.do_run = False
+        upperValueThread.join()
+        lowerValueThread = threading.Thread(target=self.constantThrottle, args=(sampleFreq, int(lowerValue)))
+        lowerValueThread.start()
+        # self.setThrottle(int(lowerValue))
+        input('SET THROTTLE RANGE: Throttle range set, press enter to turn off signal to ESC.')
+        lowerValueThread.do_run = False
+        lowerValueThread.join()
         return [lowerValue, upperValue]
         
 
+    def startMotor(self, sampleFreq, startupValue):
+        t = threading.currentThread()
+        self.startupSequence = True
+        for x in range(100):
+            value = round(x/100 * startupValue)
+            self.setThrottle(value)
+            sleep(sampleFreq)
+        print('ramp up complete')
+        while getattr(t, "do_run", True):
+            self.setThrottle(startupValue)
+            sleep(sampleFreq)
+        print('motor startup stopped')
             
+    def constantThrottle(self, sampleFreq, value):
+        t = threading.current_thread()
+        print(value)
+        while getattr(t, "do_run", True):
+            self.setThrottle(value)
+            sleep(sampleFreq)
+        # sleep(sampleFreq)
+        print(f'quit - {value}')
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import fulcrum_electronics as fe
 from database.db_manager import DBManager
+import threading
+from time import sleep
 import busio
 import board
 
@@ -10,10 +12,9 @@ sampleFreq = 0.02
 i2c_bus = busio.I2C(board.SCL, board.SDA)
 angleSensor = fe.Sensor(i2c_bus)
 db = DBManager()
-motorController = fe.MotorController(i2c_bus, 7, 500, sampleFreq, db.retrieve_fulcrum_values(id=1)[1]+500)
-
+motorController = fe.MotorController(i2c_bus, 7, 500)
 #routines
-def options():
+def options1():
         print('''
         Select your choice:
         (1): Quit
@@ -21,12 +22,28 @@ def options():
         (3): Set Throttle Range
         (4): Fulcrum Prop Values
         (5): Startup Motor
-        (6): Turn Off Motor
         '''
         )
-        optionSwitch()
+        options1Switch()
+def options2(motorThread):
+        print('''
+        Select your choice:
+        (1): Turn Off Motor
+        (2): Manual Control Mode
+        '''
+        )
+        options2Switch(motorThread)
 
-def optionSwitch():
+def options2Switch(motorThread):
+        choice = input('Choice: ')
+        if choice == '1':
+                # motorThread.do_run = False
+                # motorThread.join()
+                # motorController.stopMotor()
+                motorThread.started = False
+                options1()
+
+def options1Switch():
         choice = input('Choice: ')
         if choice == '1':
                 print('exiting')
@@ -34,32 +51,43 @@ def optionSwitch():
         elif choice == '2':
                 calibration = angleSensor.calibrate(sampleFreq)
                 db.update_calibration(calibration)
-                options()
+                options1()
         elif choice == '3':
-                [lowerValue, upperValue] = motorController.setThrottleRange()
+                [lowerValue, upperValue] = motorController.setThrottleRange(sampleFreq)
                 db.update_throttle_limits(lowerValue, upperValue)
-                options()
+                options1()
         elif choice == '4':
                 fulcrumValues = db.retrieve_fulcrum_values(id=1)
                 print(f'\nCalibration Offset: {fulcrumValues[0]}')
                 print(f'Throttle Low: {fulcrumValues[1]}')
                 print(f'Throttle High: {fulcrumValues[2]}')
                 input('\nPress return to continue...')
-                options()
+                options1()
         elif choice == '5':
-                # thread1 = fe.MotorController(i2c_bus, 7, 500)
+                # t = threading.Thread(target=motorController.startMotor, args=(sampleFreq, db.retrieve_fulcrum_values(id=1)[1]+500))
+                # t.start()
                 motorController.start()
-                # x = threading.Thread(target=motorController.startup, args=(sampleFreq, db.retrieve_fulcrum_values(id=1)[1]+500))
-                # x.start()
-                options()
-        elif choice == '6':
-                motorController.stopMotor()
-                options()
+                options2(motorController)
         else:
                 print('nothing selected, try again')
-                optionSwitch()
+                options1Switch()
+
+
+# def startMotor(sampleFreq, startupValue):
+#         t = threading.currentThread()
+#         for x in range(100):
+#                 value = round(x/100 * startupValue)
+#                 # self.channels[self.motorChannel].duty_cycle = value
+#                 motorController.setThrottle(value)
+#                 # print(value)
+#                 sleep(sampleFreq)
+#         while getattr(t, "do_run", True):
+#                 print('worked')
+#                 motorController.setThrottle(startupValue)
+#                 sleep(sampleFreq)
+
 
 def main():
-        options()
+        options1()
         pass
 main()
